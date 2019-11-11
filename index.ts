3356f0f8c8714c7d563a2ee0026f99273ede996f
Brais Antela <braantela@gmail.com>
2019-11-11 17:23:20 +0100

    Minor changes.

--- index.ts
+++ index.ts
@@ -24,6 +24,11 @@ app.use(
   })
 )
 
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to a basic express App')
+})
+
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}/api`)
 })
